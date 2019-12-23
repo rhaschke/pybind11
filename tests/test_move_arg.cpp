950c381095ad75f7ad938a3f2bda9af4fdfea96a
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2019-12-23 22:13:05 +0100

    boost::python trick for pybind11 fails

--- tests/test_move_arg.cpp
+++ tests/test_move_arg.cpp
@@ -5,8 +5,10 @@
 
 namespace py = pybind11;
 
+PYBIND11_DECLARE_HOLDER_TYPE(T, std::auto_ptr<T>)
+
 PYBIND11_MODULE(test_move_arg, m) {
-	py::class_<Item>(m, "Item")
+	py::class_<Item, std::auto_ptr<Item>>(m, "Item")
 		.def(py::init<int>(), py::call_guard<py::scoped_ostream_redirect>())
 		.def("__repr__", [](const Item& item) {
 			std::stringstream ss;
@@ -26,6 +28,13 @@ PYBIND11_MODULE(test_move_arg, m) {
 	}, py::call_guard<py::scoped_ostream_redirect>());
 #endif
 
+#if 0 // the boost::python trick, using auto_ptr's release() doesn't compile either
+	m.def("consume", [](std::auto_ptr<Item>& item) {
+		std::cout << "consume " << *item << "\n  ";
+		Item sink(std::move(*item.release()));
+		std::cout << "  old: " << item.get() << "\n  new: " << sink << "\n";
+	}, py::call_guard<py::scoped_ostream_redirect>());
+#endif
 	m.def("working", [](int&& a) { std::cout << a << "\n"; },
 	      py::call_guard<py::scoped_ostream_redirect>());
 }
